dba6f0f009d5828450ccc9b948f9506728da6781
cyn <161853405+4cyn@users.noreply.github.com>
2026-06-24 09:16:34 +0700

    lupa apa aja

--- resources/views/income/edit.blade.php
+++ resources/views/income/edit.blade.php
@@ -3,10 +3,9 @@
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">{{ __('Edit Income') }}</h2>
     </x-slot>
 
-    <div class="py-12">
+    <div class="py-12 bg-[#FFF2CC]">
         <div class="max-w-3xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
+            <div class="bg-white shadow-sm sm:rounded-lg p-6">
                     <form method="POST" action="{{ route('income.update', $income) }}">
                         @csrf
                         @method('PUT')
@@ -58,11 +57,10 @@
 
 
                         <div class="flex items-center gap-3">
-                            <button class="inline-flex items-center px-4 py-2 bg-indigo-600 text-white rounded-md">Update</button>
-                            <a href="{{ route('income.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-200 text-gray-700 rounded-md">Cancel</a>
+                            <button class="inline-flex items-center px-4 py-2 bg-[#fd593d] text-white rounded-md hover:bg-[#feaf52]">Update</button>
+                            <a href="{{ route('income.index') }}" class="inline-flex items-center px-4 py-2 bg-gray-300 text-gray-700 rounded-md hover:bg-gray-400">Cancel</a>
                         </div>
                     </form>
-                </div>
             </div>
         </div>
     </div>
